a54afa393d8fb90a1f333019e43a48dbdad0c3a7
Vasu Bansal <84651595+spooderman-x@users.noreply.github.com>
2021-05-26 07:12:56 +0530

    Create valueOfElement.cpp

---
+++ valueOfElement.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void value(int a[],int n,int k)
+{
+  cout << a[k-1];
+    
+}
+int main()
+{
+  int n;
+  cin >> n;
+  int a[n];
+  for(int i=0;i<n;i++)
+    cin >> a[i];
+  int k;
+  cin >> k;
+   value(a,n,k);
+    
+    
+    
+    return 0;
+  
+}
